ed20f3966e3f48f721139bdd058998cdbf5db401
megawatts68 <andrew.f.watts@gmail.com>
2020-09-16 02:29:31 +0700

    Server running. Migrations not.

---
+++ api/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+
+const authRouter = require('../auth/auth-router')
+
+const server = express()
+
+server.use(helmet())
+server.use(express.json())
+server.use(
+    cors({
+        origin: "*",
+        credentials: true,
+    })
+)
+
+server.use('/api', authRouter)
+
+server.get('/', (req, res) => {
+    res.json({api: 'up'})
+})
+
+module.exports = server
